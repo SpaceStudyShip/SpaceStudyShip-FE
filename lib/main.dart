d8cbbc7b14c8725d55bc018b5a7ebb85f8ce6a85
EM-H20 <ghd0106@gmail.com>
2025-12-29 12:39:08 +0900

    feat: Add Pretendard font test page
    - Replace demo counter app with font testing page
    - Display all text styles (heading1-4, body1-2, caption, overline)
    - Show all 9 font weights (Thin to Black)
    - Integrate ScreenUtilInit for responsive sizing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'core/constants/text_styles.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,57 +11,58 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) => MaterialApp(
+        title: 'Font Test',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        home: const FontTestPage(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class FontTestPage extends StatelessWidget {
+  const FontTestPage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text('Pretendard 폰트 테스트', style: AppTextStyles.heading4.bold()),
       ),
-      body: Center(
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(16.w),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text('Heading 1', style: AppTextStyles.heading1),
+            Text('Heading 2', style: AppTextStyles.heading2),
+            Text('Heading 3', style: AppTextStyles.heading3),
+            Text('Heading 4', style: AppTextStyles.heading4),
+            SizedBox(height: 20.h),
+            Text('Body 1', style: AppTextStyles.body1),
+            Text('Body 2', style: AppTextStyles.body2),
+            SizedBox(height: 20.h),
+            Text('Caption', style: AppTextStyles.caption),
+            Text('Overline', style: AppTextStyles.overline),
+            SizedBox(height: 30.h),
+            Text('Weight 테스트:', style: AppTextStyles.heading4.bold()),
+            SizedBox(height: 10.h),
+            Text('Thin (100)', style: AppTextStyles.body1.thin()),
+            Text('ExtraLight (200)', style: AppTextStyles.body1.extraLight()),
+            Text('Light (300)', style: AppTextStyles.body1.light()),
+            Text('Regular (400)', style: AppTextStyles.body1.regular()),
+            Text('Medium (500)', style: AppTextStyles.body1.medium()),
+            Text('SemiBold (600)', style: AppTextStyles.body1.semiBold()),
+            Text('Bold (700)', style: AppTextStyles.body1.bold()),
+            Text('ExtraBold (800)', style: AppTextStyles.body1.extraBold()),
+            Text('Black (900)', style: AppTextStyles.body1.black()),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }

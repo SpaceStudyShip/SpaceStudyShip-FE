a870a246ee5a63fbd5a711281f8f204b4c5db682
EM-H20 <ghd0106@gmail.com>
2026-03-04 20:02:42 +0900

    feat : SettingsRepository 인터페이스 정의

---
+++ lib/features/settings/domain/repositories/settings_repository.dart
@@ -0,0 +1,6 @@
+import '../entities/app_settings_entity.dart';
+
+abstract class SettingsRepository {
+  AppSettingsEntity getSettings();
+  Future<void> setStarTwinkleEnabled({required bool enabled});
+}

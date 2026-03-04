299c791680fef5c2cefa5fb3d5518519f2404c00
EM-H20 <ghd0106@gmail.com>
2026-03-05 08:50:59 +0900

    refactor: Settings DataSource/Repository provider를 data 레이어로 분리 #46

---
+++ lib/features/settings/data/providers/settings_data_providers.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'settings_data_providers.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$settingsLocalDataSourceHash() =>
+    r'e1785bd38ba66083904adecd6167992d5c4db412';
+
+/// See also [settingsLocalDataSource].
+@ProviderFor(settingsLocalDataSource)
+final settingsLocalDataSourceProvider =
+    AutoDisposeProvider<SettingsLocalDataSource>.internal(
+      settingsLocalDataSource,
+      name: r'settingsLocalDataSourceProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$settingsLocalDataSourceHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SettingsLocalDataSourceRef =
+    AutoDisposeProviderRef<SettingsLocalDataSource>;
+String _$settingsRepositoryHash() =>
+    r'64bdbd4e731ae5defba90b5a302432d3e2b4386b';
+
+/// See also [settingsRepository].
+@ProviderFor(settingsRepository)
+final settingsRepositoryProvider =
+    AutoDisposeProvider<SettingsRepository>.internal(
+      settingsRepository,
+      name: r'settingsRepositoryProvider',
+      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+          ? null
+          : _$settingsRepositoryHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+@Deprecated('Will be removed in 3.0. Use Ref instead')
+// ignore: unused_element
+typedef SettingsRepositoryRef = AutoDisposeProviderRef<SettingsRepository>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
--- lib/features/settings/presentation/providers/settings_provider.g.dart
+++ lib/features/settings/presentation/providers/settings_provider.g.dart
@@ -6,45 +6,6 @@ part of 'settings_provider.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$settingsLocalDataSourceHash() =>
-    r'e1785bd38ba66083904adecd6167992d5c4db412';
-
-/// See also [settingsLocalDataSource].
-@ProviderFor(settingsLocalDataSource)
-final settingsLocalDataSourceProvider =
-    AutoDisposeProvider<SettingsLocalDataSource>.internal(
-      settingsLocalDataSource,
-      name: r'settingsLocalDataSourceProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$settingsLocalDataSourceHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef SettingsLocalDataSourceRef =
-    AutoDisposeProviderRef<SettingsLocalDataSource>;
-String _$settingsRepositoryHash() =>
-    r'64bdbd4e731ae5defba90b5a302432d3e2b4386b';
-
-/// See also [settingsRepository].
-@ProviderFor(settingsRepository)
-final settingsRepositoryProvider =
-    AutoDisposeProvider<SettingsRepository>.internal(
-      settingsRepository,
-      name: r'settingsRepositoryProvider',
-      debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$settingsRepositoryHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
-
-@Deprecated('Will be removed in 3.0. Use Ref instead')
-// ignore: unused_element
-typedef SettingsRepositoryRef = AutoDisposeProviderRef<SettingsRepository>;
 String _$getSettingsUseCaseHash() =>
     r'09c503ca87f7a3fdda8750ca6f256cd5ef800f73';
 
